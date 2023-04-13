b3fb40d72ac4178df204c68fc56ddb2ccd42245b
Diogo-V <diogo.venancio00@gmail.com>
2023-04-13 16:22:33 +0100

    chore: almost finished

--- src/main.c
+++ src/main.c
@@ -132,31 +132,37 @@ void free_program_memory() {
 
 
 void traverse_tree(Node p, int n, char* T) {
-    // If p is NULL, return
+
+    Node child = NULL;
+
+    /* Initialize the smallest child to NULL and the smallest character to '~' (larger than any character) */
+    Node smallestChild = NULL;
+    char smallestChar = '~';
+
+    /* If p is NULL, return */
     if (p == NULL) {
         return;
     }
-    // If string depth of p is equal to n, print the string found
+
+    /* If string depth of p is equal to n, print the string found */
     if (p->depth == n) {
         printf("%.*s\n", n, &T[p->head]);
         return;
     }
-    // Initialize the smallest child to NULL and the smallest character to '~' (larger than any character)
-    Node smallestChild = NULL;
-    char smallestChar = '~';
-    // Traverse all children of p
-    for (Node child = p->child; child != NULL; child = child->brother) {
-        // If the first character of the label of the child is smaller than smallestChar, update smallestChar and smallestChild
+
+    /* Traverse all children of p */
+    for (child = p->child; child != NULL; child = child->brother) {
+
+        /* If the first character of the label of the child is smaller than smallestChar, update smallestChar and smallestChild */
         if (T[child->head] < smallestChar) {
             smallestChar = T[child->head];
             smallestChild = child;
         }
     }
-    // Recursively traverse the subtree rooted at the smallest child
+
+    /* Recursively traverse the subtree rooted at the smallest child */
     traverse_tree(smallestChild, n, T);
 }
-
-
 
 
 void print_final() {
@@ -174,7 +180,9 @@ void print_final() {
 
 int DescendQ(Point point, char string_char) {
 
-    if (point->above == &nodes[1]) { // special case for sentinel
+    Node n = NULL;
+
+    if (point->above == &nodes[1]) { /* Special case for sentinel */
         return 1;
     }
 
@@ -182,7 +190,7 @@ int DescendQ(Point point, char string_char) {
         return string[point->below->head + point->depth] == string_char;
     }
 
-    Node n = point->below->child;
+    n = point->below->child;
     while (n != NULL) {
         if (string[n->head + point->above->depth] == string_char) {
             return 1;
@@ -190,53 +198,35 @@ int DescendQ(Point point, char string_char) {
         n = n->brother;
     }
 
-//    Node below = point->below;
-//    int below_head = below->head;
-//
-//    while (below_head < n_tree_nodes && below_head <= below->head + below->depth) {
-//        if (string[below_head] == string_char) {
-//            return 1;
-//        }
-//        below_head++;
-//    }
-
     return 0;
 }
 
-// Quando faço descend no SuffixLink, vou ter de corrigir o point.depth com o nó depth e a apontar para o nó de baixo
-
-// Chamo SuffixLink na raiz vai para sentinela
-// Suffix link da raiz é a sentinela
-// O point tem o above e o below igual ao nó em que estou
-// Quando chamo o AddLeaf, se for a criar um nó interno, coloco o suffix do last a apontar para o novo nó e depois mudo o last para o novo nó criado
-// AddLeaf adds at most 2 nodes
 
 void Descend(Point p, char c) {
-    if (p->above == &nodes[1]) { // special case for sentinel
+    Node n = NULL;
 
+    if (p->above == &nodes[1]) { /* special case for sentinel */
         p->below = &nodes[0];
         p->above = &nodes[0];
         p->depth = 0;
         return;
-
     }
 
-    // FIXME: Quando o above e o below do point são diferentes, tenho de verificar se posso descer
-    if (p->above != p->below) {  // TODO: refactor this
+    if (p->above != p->below) {
         if (string[p->below->head + p->depth] == c) {
             p->depth++;
 
-            if (p->depth == p->below->depth) {  // Passei a estar no nó
+            if (p->depth == p->below->depth) {  /* Is now in node */
                 p->above = p->below;
             }
 
-        }  // Head do de baixo + depth do de cima
+        }  /* Head of below node + depth of above node */
     }
 
-    // Finds branch to descend at
-    Node n = p->below->child;
+    /* Finds branch to descend at */
+    n = p->below->child;
     while (n != NULL) {
-        if (string[n->head + p->above->depth] == c) {  // Head do de baixo + depth do de cima
+        if (string[n->head + p->above->depth] == c) {
             p->depth++;
             p->below = n;
             if (p->depth == p->below->depth) {
@@ -247,45 +237,23 @@ void Descend(Point p, char c) {
         n = n->brother;
     }
 
-//    Node u = p->below;
-//    int i = u->head;
-//
-//    while (i < n_nodes && i <= u->head + u->depth) {
-//        if (string[i] == c) {
-//            p->depth++;
-//            return;
-//        }
-//        i++;
-//    }
-//
-//    p->below = u->brother;
-//    p->depth = u->depth + 1;
-//
-//    while (p->below != NULL && p->below->head + p->depth >= n_nodes) {
-//        p->below = p->below->suffix_link;
-//        p->depth = p->below->depth;
-//    }
-
 }
-
-// Se uma leaf for adicionada, não interessa. Apenas nós internos são relevantes
 
 
 int AddLeaf(Point p, Node string_idx_node, int string_idx) {
-    int n_nodes_added = 0; // number of nodes added
+    int n_nodes_added = 0;
     int next_node = 2 + n_tree_nodes;
 
     Node parent = p->above;
     int depth = p->depth;
 
-    // If the pointer is not at a leaf, then we need to add an internal node
+    /* If the pointer is not at a leaf, then we need to add an internal node */
     if (p->depth + 1 <= p->below->depth) {
 
-        // split the edge and create n_nodes_added new internal node
+        /* Split the edge and create n_nodes_added new internal node */
         n_nodes_added++;
 
         nodes[next_node].head = p->below->head;
-        // nodes[next_node].depth = depth + q->depth - k;
         nodes[next_node].depth = depth;
         nodes[next_node].child = p->below;
         nodes[next_node].brother = p->below->brother;
@@ -294,16 +262,12 @@ int AddLeaf(Point p, Node string_idx_node, int string_idx) {
             p->below->brother->hook = &nodes[next_node].brother;
         }
 
-        nodes[next_node].suffix_link = NULL;  // Suffix links is parent node
+        nodes[next_node].suffix_link = NULL;  /* Suffix links is parent node */
         nodes[next_node].hook = p->below->hook;
 
         *nodes[next_node].hook = &nodes[next_node];
 
-        p->below->brother = NULL;  // Passa a ser leaf
-
-        parent->child = &nodes[next_node];
-
-        // FIXME: Acho que não estou a colocar bem os nós brother e child
+        p->below->brother = NULL;  /* Passa a ser leaf */
 
         /* Updates last's suffix to the newly created internal node */
         if (last != NULL) {
@@ -318,55 +282,7 @@ int AddLeaf(Point p, Node string_idx_node, int string_idx) {
 
     }
 
-//    while(depth <= p->below->depth) {  // Internal nodes
-//
-//        // find the edge to descend on
-//        Node q = parent->child;
-//        while(string[q->head] != string[depth]) {
-//            q = q->brother;
-//        }
-//
-//        int k = q->head;
-//
-//        // if T[k+1]...T[q->sdep] is n_nodes_added prefix of T[i+1]...T[n]
-//        if(depth + q->depth - k <= p->below->depth) {
-//            parent = q;
-//            depth = q->depth;
-//        } else {
-//
-//            // split the edge and create n_nodes_added new internal node
-//            n_nodes_added++;
-//
-//            nodes[next_node].head = k;
-//            // nodes[next_node].depth = depth + q->depth - k;
-//            nodes[next_node].depth = p->depth;
-//            nodes[next_node].child = q;
-//            nodes[next_node].brother = parent->child;
-//            nodes[next_node].suffix_link = NULL;  // Suffix links is parent node
-//            nodes[next_node].hook = &string_idx_node;
-//
-//            parent->child = &nodes[next_node];
-//
-//            parent = &nodes[next_node];
-//            depth = nodes[next_node].depth;
-//
-//            /* Updates last's suffix to the newly created internal node */
-//            if (last != NULL) {
-//                last->suffix_link = &nodes[next_node];
-//            }
-//            last = &nodes[next_node];
-//
-//            n_nodes_added++;
-//            printf("Internal ");
-//            show_node(&nodes[2 + n_tree_nodes + n_nodes_added]);
-//        }
-//    }
-
-    // create n_nodes_added new leaf node
-    // if(s != NULL) {
-    //     s->suffix_link = parent;
-    // }
-
+    /* Leaf nodes */
     next_node = next_node + n_nodes_added;
 
     nodes[next_node].head = string_idx - p->depth;
@@ -391,30 +307,40 @@ int AddLeaf(Point p, Node string_idx_node, int string_idx) {
  */
 void SuffixLink(Point p) {
 
-    /* Update last_internal_node to be the current node p->above */
-    last = p->above;
+    int head = 0;
+    int depth = 0;
 
-    /* If last_internal_node is the root node, put point in sentinel and return */
-    if (last == &nodes[0]) {
-        p->above = &nodes[1];
-        p->below = &nodes[1];
+    /* If only created leaf, then goes to parent's suffix link */
+    if (p->above == p->below) {
+        p->above = p->above->suffix_link;
+        p->below = p->above;
+        p->depth = p->above->depth;
         return;
     }
 
+    /* Stores initial head to be used in the left branch */
+    head = p->below->head;
+    depth = p->depth - 1;
+
     /* Move up one level in the tree */
-    p->above = last->suffix_link;
+    p->above = p->above->suffix_link;
+    p->below = p->above;
+    p->depth = p->above->depth;
 
-    /* Continue moving up the tree while p->above is not the root and the edge from p->above to p->below is a leaf edge */
-    while (p->above != &nodes[0] && p->above->child == NULL) {
-        p->above = p->above->suffix_link;
+    /* string[head + p->depth + 1] == string[p->below->head + p->above->depth] */
+    while (p->depth < depth) {
+        p->above = p->below;
+        Descend(p, string[head + p->depth + 1]);
+        p->depth = p->below->depth;
+    }
+    p->depth = depth;
+
+    /* Update last_internal_node to be the current node p->above */
+    if (p->below == p->above && last != NULL) {
+        last->suffix_link = p->above;
+        last = NULL;
     }
 
-    /* If p->above is not the root, move down the tree to the first child of p->above that matches the first character of the edge from p->above to p->below */
-    if (p->above != &nodes[0]) {
-        Descend(p, string[p->below->head]);
-        // p->above = p->below;
-        // p->depth = p->below->depth;
-    }
 }
 
 /**
@@ -423,7 +349,7 @@ void SuffixLink(Point p) {
 void ukkonen_algorithm() {
 
     int string_idx = 0;
-    Point point = (Point) calloc(1, sizeof(struct point)); // create a new point
+    Point point = (Point) calloc(1, sizeof(struct point));
 
     /* Builds initial point */
     point->above = &nodes[0];
@@ -435,13 +361,13 @@ void ukkonen_algorithm() {
 
         printf("Letter %c\n", '\0' == string[string_idx] ? '$' : string[string_idx]);
 
-        // Check if there is a child node that matches the current character
+        /* Check if there is a child node that matches the current character */
         while(!DescendQ(point, string[string_idx])) {
             n_tree_nodes += AddLeaf(point, &(nodes[n_tree_nodes]), string_idx);
-            SuffixLink(point);  // Vai-me colocar na sentinela e depois o Descend coloca-me na root
+            SuffixLink(point);
         }
 
-        // Move down the tree to the child node that matches the current character
+        /* Move down the tree to the child node that matches the current character */
         Descend(point, string[string_idx]);
 
         string_idx++;
@@ -481,6 +407,7 @@ void init_nodes() {
 
     /* Root's suffix link is the sentinel */
     nodes[0].suffix_link = &nodes[1];
+    nodes[1].depth = -1;
 
     free(tmp);
     free(input);
@@ -500,10 +427,10 @@ int main() {
     ukkonen_algorithm();
 
     /* Traverses all nodes and outputs final string */
-    // traverse_tree(nodes, n_nodes, string);
+    /* traverse_tree(nodes, n_nodes, string); */
 
     /* Prints all node's final state */
-    // print_final();
+    /* print_final(); */
 
     /* Cleans up the program by freeing all the allocated memory */
     free_program_memory();
